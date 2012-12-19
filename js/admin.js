702342eea404d96bd8e99c4328155b29640bda12
Simon Fransson <simon@dessibelle.se>
2012-12-19 18:24:37 +0100

    Minor js fix

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-   
+
    /* Example patterns */
    $('a#any_hostname_all_link, a#any_hostname_dotcom_link').click(function(e) {
 
@@ -7,45 +7,46 @@ jQuery(document).ready(function($) {
         appendHost($(this).text());
    });
 
-   /* Append hsot to textarea */
+   /* Append host to textarea */
    $('a#any_hostname_add_host_link').click(function(e) {
-       
+
        e.preventDefault();
-       
+
        var val = $('input#any_hostname_add_host_field').val();
        val = val.replace(/\./g, "\\.");
-       
+
        appendHost(val);
-       
+
        $('input#any_hostname_add_host_field').val(null);
    });
-   
+
    /* Display warning if current host is not in list */
    $('input#submit').closest('form').submit(function(e) {
-       
+
        var patterns = $.trim($('textarea#any_hostname_allowed_hosts').val()).split("\n");
        var current_host = window.location.host;
-       
+
+       var idx;
        var current_host_ok = false;
        for (idx in patterns) {
            var p = patterns[idx];
            var m = current_host.match(new RegExp(p));
-           
+
            current_host_ok = current_host_ok || (m == current_host);
        }
-       
+
        if (!current_host_ok) {
            var host_warning = $('#any_hostname_host_warning').text();
            return confirm(host_warning);
        }
-       
+
        return true;
    });
 });
 
 function appendHost(host) {
     $ = jQuery;
-    
+
     var val = $.trim($('textarea#any_hostname_allowed_hosts').val());
     $('textarea#any_hostname_allowed_hosts').val(val + "\n" + host);
 }
